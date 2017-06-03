60cc168278097cfa86a528c19e49db1aa34c400b
nairx737 <nairi.sargsyan@gmail.com>
2017-06-03 18:56:21 +0400

    1

---
+++ src/result.java
@@ -0,0 +1,67 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.oracle.jrockit.jfr.Transition.From;
+import static com.oracle.jrockit.jfr.Transition.To;
+
+/**
+ * Created by Nairi.Sargsyan on 02.06.17.
+ */
+@WebServlet(name = "result")
+public class result extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out= response.getWriter();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String FlyFrom= request.getParameter("FlyFrom");
+        String FlyTO=request.getParameter("FlyTO");
+        String Departing=request.getParameter("Departing");
+        String Price=request.getParameter("Price");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date DepartingDate= sdf.parse(Departing);
+            out.print(DepartingDate+"<br>");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Airport", "root", "00410033");
+            Statement statement = connection.createStatement();
+            String query="INSERT INTO airport.flights(From, To, Day, Price)" +
+                    " VALUES ("+FlyFrom+","+FlyTO+","+Departing+","+Price+")";
+            statement.executeUpdate(query);
+
+//            ResultSet resultSet = statement.executeQuery("SELECT seller FROM shop");
+//            String s = "";
+//            while (resultSet.next()) {
+//                s += resultSet.getString("seller") + "<br>";
+//            }
+//            out.println(s);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ web/addFlight.jsp
@@ -0,0 +1,23 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Home</title>
+      <link href="styleAddFlight.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <div class="container">
+    <form method="post" action="result">
+      <input type="text" name="FlyFrom" placeholder="Flying from">
+      <input type="text" name="FlyTo" placeholder="Fly to">
+      <input type="date" name="Departing" placeholder="Departing day">
+      <input type="date" name="Returning" placeholder="Returning day">
+      <input type="text" name="Price" placeholder="Price in USD">
+      <input type="submit" value="submit">
+
+
+    </form>
+  </div>
+
+  </body>
+</html>
